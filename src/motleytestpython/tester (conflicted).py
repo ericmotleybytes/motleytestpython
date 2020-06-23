76ed1aa860500bf95268b781078a45183dabd111
Eric Christiansen <eric@motleybytes.com>
2020-06-23 13:15:41 -0700

    Commit.

--- src/motleytestpython/tester (conflicted).py
+++ src/motleytestpython/tester (conflicted).py
@@ -3,8 +3,6 @@ import unittest
 import sys
 import operator
 import datetime
-import random
-import math
 class Tester(unittest.TestCase):
 
     def assert_equal(self,expected,actual,message=None):
@@ -215,27 +213,10 @@ class Tester(unittest.TestCase):
         self.assertFalse(22 in set((1,2,3)))
 
     def test_complex(self):
-        self.assert_equal(2.0,complex(2,3).real)
-        self.assert_equal(3.0,complex(2,3).imag)
-        self.assert_equal(2.1,complex(2.1,3.1).real)
-        self.assert_equal(3.1,complex(2.1,3.1).imag)
-        c = 2.1 + 3.1j
-        self.assert_equal(2.1,c.real)
-        self.assert_equal(3.1,c.imag)
-        self.assert_equal(2.1,c.conjugate().real)
-        self.assert_equal(-3.1,c.conjugate().imag)
-        c = (2+3j)+(3-2j)
-        self.assert_equal(5.0,c.real)
-        self.assert_equal(1.0,c.imag)
-        c = (2+3j)-(3-2j)
-        self.assert_equal(-1.0,c.real)
-        self.assert_equal(5.0,c.imag)
-        c = (2+3j)*(3-2j)
-        self.assert_equal(12.0,c.real)
-        self.assert_equal(5.0,c.imag)
-        c = (2+3j)/(3-2j)
-        self.assert_equal(0.0,c.real)
-        self.assert_equal(1.0,c.imag)
+        self.assert_equal(2,real(complex(2,3)))
+        self.assert_equal(3,complex(2,3).imag())
+        self.assert_equal(2.0,complex(2,3).real())
+        self.assert_equal(3.0,complex(2,3).imag())
 
     def test_class_names(self):
         i = 42
@@ -258,66 +239,6 @@ class Tester(unittest.TestCase):
         self.assert_equal('set', x.__class__.__name__)
         x = frozenset({1,2,3})
         self.assert_equal('frozenset', x.__class__.__name__)
-        x = bytearray(10)
-        self.assert_equal('bytearray', x.__class__.__name__)
-        x = bytes(5)
-        self.assert_equal('bytes', x.__class__.__name__)
-        x = memoryview(bytes(5))
-        self.assert_equal('memoryview', x.__class__.__name__)
-
-    def test_random(self):
-        random.seed()
-        for i in range(0,200):
-            x = random.randrange(1,10,1)  # random int from 1 to 9 inclusive.
-            self.assertTrue(x>=1 and x<=9)
-
-    def test_type_casting(self):
-        self.assert_equal(1,int(1.9))
-        self.assert_equal(-1,int(-1.9))
-        self.assert_equal(2,int("2"))
-        self.assert_equal(1.0,float(1))
-        self.assert_equal(2.5,float("2.5"))
-        self.assert_equal("2",str(2))
-        self.assert_equal("2.5",str(2.5))
-        self.assert_equal("(2+3j)",str((2+3j)))
-
-    def test_exponential_notation(self):
-        self.assert_equal(35000.0,3.5e4)
-        self.assert_equal(2000.0,2e3)
-        self.assert_equal(35000.0,3.5E4)
-        self.assert_equal(0.35,3.5E-1)
-
-    def test_math_functions(self):
-        self.assert_equal(5,abs(5))
-        self.assert_equal(5,abs(-5))
-        self.assert_equal(5.0,abs(-5.0))
-        self.assert_equal(8,pow(2,3))
-        self.assert_equal(8.0,pow(2.0,3.0))
-        self.assert_equal(42,round(42.3))
-        self.assert_equal(42,round(41.5))
-        self.assert_equal(42,round(41.7))
-        self.assert_equal(-42,round(-42.3))
-        self.assert_equal(-42,round(-41.5))
-        self.assert_equal(-42,round(-41.7))
-        self.assert_equal(3.1416,round(3.14159,4))
-        self.assert_equal(99,max(3, 99, 23.3))
-        self.assert_equal(99,max([3, 99, 23.3]))
-        self.assert_equal(3,min(3, 99, 23.3))
-        self.assert_equal(3,min([3, 99, 23.3]))
-        self.assert_equal(3,math.floor(3.9))
-        self.assert_equal(-4,math.floor(-3.9))
-        self.assert_equal(3,math.ceil(2.1))
-        self.assert_equal(-3,math.ceil(-3.9))
-        self.assert_equal(3.0, math.fabs(-3))
-        self.assert_equal(3.0, math.fabs(-3))
-        self.assert_equal(0.5, math.sqrt(0.25))
-        self.assert_equal(3.14159, round(math.pi,5))
-        self.assert_equal(2.71828, round(math.e,5))
-        self.assert_equal(0.6931, round(math.log(2),4))
-        self.assert_equal(1.0, math.sin(math.pi/2))
-        self.assert_equal(1.0, math.cos(0))
-        self.assert_equal(90.0, math.degrees(math.pi/2))
-        self.assert_equal(math.pi/2, math.radians(90))
 
     def test_str_concat(self):
         self.assert_equal("abcxyz","abc"+"xyz")
